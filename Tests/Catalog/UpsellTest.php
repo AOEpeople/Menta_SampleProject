979e81711ab0ca3068b92d7020822b4384463aef
Agata <a.firlejczyk@macopedia.pl>
2014-01-26 12:19:25 +0100

    UpsellTest - check upsell products present on product view page

---
+++ Tests/Catalog/UpsellTest.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once dirname(__FILE__) . '/../TestcaseAbstract.php';
+
+class Acceptance_Tests_Catalog_UpsellTest extends TestcaseAbstract
+{
+    /**
+     * Check upsell products present on product view page
+     *
+     * @test
+     * @return void
+     */
+    public function checkUpsellProductsPresent()
+    {
+        /* @var $cart MagentoComponents_Pages_Cart */
+        $cart = Menta_ComponentManager::get('MagentoComponents_Pages_Cart');
+        $cart->clearCart();
+
+        /* @var $singleView MagentoComponents_Pages_ProductSingleView */
+        $singleView = Menta_ComponentManager::get('MagentoComponents_Pages_ProductSingleView');
+        $singleView->openProduct(126);
+        $this->assertTextPresent('The Only Children: Paisley T-Shirt');
+
+        $this->assertElementPresent('id=product-price-39-upsell');
+        $this->assertElementPresent('id=product-price-119-upsell');
+        $this->assertElementPresent('id=product-price-123-upsell');
+
+        // put one of the upsells into the cart
+        $singleView->putProductsIntoCart(39);
+
+        // after reloading the page the product should be  gone from upsells
+        $singleView->openProduct(126);
+        $this->assertElementPresent('id=product-price-119-upsell');
+        $this->assertElementPresent('id=product-price-123-upsell');
+    }
+}
