5864eb20a5ce509ea7719c890aa1162c3c93bdb2
Agata Firlejczyk <afirlejczyk@gmail.com>
2015-05-15 20:16:19 +0200

    Add new test for magento 1.9 - testing Admin Dashboard Page

---
+++ Tests/Admin/UsernameTest.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once dirname(__FILE__) . '/../TestcaseAbstract.php';
+
+/**
+ * Admin username tests
+ */
+class Tests_Admin_UsernameTest extends TestcaseAbstract
+{
+    /**
+     * Check link in header
+     * @test
+     */
+    public function loginWhenNotLoggedIn()
+    {
+        $admin = Menta_ComponentManager::get('MagentoComponents_Pages_Admin');
+        /* @var $admin MagentoComponents_Pages_Admin */
+        $admin->openAdmin();
+        $adminUser = $admin->getAdminUser();
+        $admin->logIntoAdmin($adminUser['username'], $adminUser['password']);
+        $admin->loginCheck();
+        $admin->logoutFromAdmin();
+    }
+
+    /**
+     * Check user name when logged in
+     * @test
+     */
+    public function userNameWhenLoggedIn()
+    {
+        $admin = Menta_ComponentManager::get('MagentoComponents_Pages_Admin');
+        /* @var $admin MagentoComponents_Pages_Admin */
+        $admin->openAdmin();
+        $adminUser = $admin->getAdminUser();
+        $admin->logIntoAdmin($adminUser['username'], $adminUser['password']);
+
+        $this->getHelperAssert()->assertElementContainsText('//div[' . Menta_Util_Div::contains('header'). ']',
+            $adminUser['username']);
+
+        $admin->logoutFromAdmin();
+    }
+}
